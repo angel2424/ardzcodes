ff2a49564c4931a41ff4cc0ff25fce5bc45e2feb
Angel Rodriguez <angel.rodriguez@cimagroup.com.mx>
2025-10-13 17:13:20 -0500

    Fixed portrait animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import { defineConfig } from "astro/config"
 
 import tailwindcss from "@tailwindcss/vite"
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
-	i18n: {
-		locales: ["en", "es"],
-		defaultLocale: "en",
+
+  i18n: {
+      locales: ["en", "es"],
+      defaultLocale: "en",
 	},
+
+  integrations: [react()],
 })
